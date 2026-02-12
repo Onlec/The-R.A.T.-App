ed2b338f39e167cf1c65f4ca8c9b44ce02d483ec
Onlec <willemcallebert@gmail.com>
2026-02-12 15:01:25 +0100

    logo

--- The_R.A..T._App.py
+++ The_R.A..T._App.py
@@ -42,7 +42,7 @@ options = [HORIZONTAL_RED, ICON_RED, HORIZONTAL_BLUE, ICON_BLUE]
 sidebar_logo = st.selectbox("Sidebar logo", options, 0)
 main_body_logo = st.selectbox("Main body logo", options, 1)
 
-st.logo(sidebar_logo, icon_image=main_body_logo)
+st.logo(sidebar_logo, size="medium", icon_image=main_body_logo)
 st.sidebar.markdown("Hi!")
 
 
